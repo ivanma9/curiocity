592aa153c9eb19e1e38a443e3195cbac270af3e8
Ivan <ivanma819@gmail.com>
2022-02-05 14:29:44 -0800

    Create server

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const PORT = 8080;
-app.listen(
-  PORT,
-  () => console.log("it's alive")
-)
+app.listen(PORT, () => console.log("it's alive"));
 
-app.get('/tshirt', (req,res) => {
+app.use(express.json());
 
-})
+//routes
+app.get("/user", (req, res) => {
+	res.status(200).send({
+		name: "Josh",
+		password: "bingBong",
+	});
+});
+
+app.post("/user/:id", (req, res) => {
+	const { id } = req.params;
+	const { favColor } = req.body;
+
+	if (!favColor) {
+		res.status(418).send({ message: "We need a favorite Color!" });
+	}
+
+	res.send({
+		name: `Bobby with your ${favColor} and ID of ${id}`,
+	});
+});
