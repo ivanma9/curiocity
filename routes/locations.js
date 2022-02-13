d78b65462cf14a593849baf5ef85c38ff46380ca
rohans30 <contactrohans@gmail.com>
2022-02-13 15:56:37 -0800

    Added get/post for locations db

--- routes/locations.js
+++ routes/locations.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const req = require("express/lib/request");
 
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
@@ -9,53 +10,58 @@ const locationRoutes = express.Router();
 const dbo = require("../db/mongoConn");
 
 // This section will help you get a list of all the locations.
-locationRoutes.route("/location").get(async function (_req, res) {
+locationRoutes.route("/location").get(function (req, res) {
 	const dbConnect = dbo.getDb();
 
 	dbConnect
 		.collection("locations")
-		.find({})
-		.limit(50)
+		.find({name: req.body.name})
 		.toArray(function (err, result) {
 			if (err) {
 				res.status(400).send("Error fetching listings!");
+			} else if (result.length == 0) {
+				res.status(400).send(`No locations with name ${req.body.name}`);
 			} else {
 				res.json(result);
 			}
 		});
+});
 
+locationRoutes.route("/insert").post(function (req, res) {
+	const dbConnect = dbo.getDb();
 
-        
-	// // await client.connect();
+	const location = {
+		name: req.body.name,
+		tags: req.body.tags,
+		city: req.body.city,
+	};
 
-	// const locations_collection = client
-	// 	.db("sample_locations")
-	// 	.collection("locations");
-	// const loc_name = "The Getty";
-	// const loc_tags = ["Museum", "Art"];
-	// const loc_city = "Santa Monica";
-
-	// const cursor = locations_collection.find({ name: loc_name });
-
-	// const loc = cursor.next();
-
-	// loc.then((l) => {
-	// 	if (l) {
-	// 		console.log(`Already found location named ${loc_name}`);
-	// 		console.log(l);
-	// 	} else {
-	// 		console.log(`Inserting ${loc_name}`);
-	// 		locations_collection.insertOne(
-	// 			{ name: loc_name, tags: loc_tags, city: loc_city },
-	// 			(err, data) => {
-	// 				if (err) return console.log(err);
-	// 			}
-	// 		);
-	// 	}
-	// });
-
-
-
+	dbConnect
+		.collection('locations')
+		.find({name: req.body.name})
+		.toArray(function (err, result) {
+			if (err) {
+				res.status(400).send("Error fetching listings!");
+			} else if (result.length != 0){
+				console.log(`Already found location named ${req.body.name}`)
+				console.log(result);
+				// res.json(result);
+				res.status(400).send('Error inserting matches!');
+			} else {
+				dbConnect
+					.collection("locations").insertOne(location, function (err, result) {
+						if (err) {
+							res.status(400).send('Error inserting matches!');
+						} else {
+							console.log(`Added a new match with id ${result.insertedId}`);
+							// res.json(result);
+							res.json(location);
+							res.status(204).send();
+						}
+					});
+			}
+		});
+		
 });
 
 module.exports = locationRoutes;
