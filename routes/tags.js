917d6e4f47c5abbbcbf8d1e8255f745392942d66
kunalpatil01 <98849253+kunalpatil01@users.noreply.github.com>
2022-02-24 13:44:11 -0800

    Update tags.js

--- routes/tags.js
+++ routes/tags.js
@@ -1,5 +1,5 @@
-const { query } = require('express');
 const express = require('express');
+const req = require("express/lib/request");
 
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
@@ -10,10 +10,24 @@ const tagRoutes = express.Router();
 const dbo = require('../db/mongoConn');
 
 // This section will help you get a list of all the records.
-tagRoutes.route('/tags').get(async function (_req, res) {
+tagRoutes.route('/tags').get(async function (req, res) {
   const dbConnect = dbo.getDb();
   const collection = dbConnect.db('businesses').collection('tags')
+  const category = req.body.category;
 
+  if(category){
+    collection
+    .find({category:category})
+    .limit(50)
+    .toArray(function (err, result) {
+      if (err) {
+        res.status(400).send('Error fetching tags!');
+      } else {
+        res.json(result);
+      }
+  })
+  }
+  else{
     collection
     .find({})
     .limit(50)
@@ -24,63 +38,31 @@ tagRoutes.route('/tags').get(async function (_req, res) {
         res.json(result);
       }
   })
+}
+   
   
 });
 
 
-tagRoutes.route('/insert').post(function (req, res) {
+tagRoutes.route('/insert/tag').post(function (req, res) {
   const dbConnect = dbo.getDb();
+  const collection = dbConnect.db('businesses').collection('tags')
   const tag = {
     name: req.body.name,
     category: req.body.category
   };
 
-  dbConnect
-    .collection('tags')
+  collection
     .insertOne(tag, function (err, result) {
       if (err) {
         res.status(400).send('Error inserting tag!');
       } else {
         console.log(`Added a new tag with id ${result.insertedId}`);
-        res.status(204).send();
+        res.status(204).send(`Added a tag with name: ${req.body.name} and category ${req.body.category} `);
         res.json(tag);
       }
     });
 });
 
-
-tagRoutes.route('/tag/update/:id').put((req, res, next) =>{//update/edit a tag by ID
-  const dbConnect = dbo.getDb();
-  let id = {
-    _id: ObjectId(req.params.id)
-  };
-  if(req.body.category.length != 0 && req.body.name.length != 0){
-    dbConnect.collection("tags").update({_id: id}, {$set:{'category': req.body.category, 'name': req.body.name}}, (err, result) => {
-      if(err) {
-        throw err;
-      }
-      res.send('tag updated sucessfully');
-    });
-  }
-  else if(req.body.category.length != 0){
-    dbConnect.collection("tags").update({_id: id}, {$set:{'category': req.body.category}}, (err, result) => {
-      if(err) {
-        throw err;
-      }
-      res.send('tag category updated sucessfully');
-  })
-}
-else if(req.body.name.length != 0){
-  dbConnect.collection("tags").update({_id: id}, {$set:{'name': req.body.name}}, (err, result) => {
-    if(err) {
-      throw err;
-    }
-    res.send('tag name updated sucessfully');
-}
-)}
-else{
-  res.status(400).send("We need a tag or category to update!");
-}
-});
 
 module.exports = tagRoutes;
