e4da264f455a2b33a8ccd706f7d40048e8a91ead
Kunal Patil <patil.kunal919@gmail.com>
2022-05-28 19:32:55 -0700

    Fix minor problems with locations routes

--- routes/locations.js
+++ routes/locations.js
@@ -136,7 +136,7 @@ locationRoutes.route("/distance").get(function(req, res){
 
 	const long = parseFloat(req.query.longitude);
 	const lat = parseFloat(req.query.latitude);  
-	const radius_in_miles = req.query.radius;
+	const radius_in_miles = parseFloat(req.query.radius);
 	const radius = 1609.34*radius_in_miles;
 
 	const url = s`Hey name is ${req.query.name}`;
@@ -147,7 +147,7 @@ locationRoutes.route("/distance").get(function(req, res){
 				$near:
 				{
 					$geometry: {type: "Point", coordinates: [long, lat]},
-					$maxDistance: parseFloat(radius)
+					$maxDistance: radius
 				}
 			}}
 		).toArray(function (err, result) {
@@ -174,7 +174,7 @@ locationRoutes.route("/walking").get(function(req, res){
 	const long = parseFloat(req.query.longitude);
 	const lat = parseFloat(req.query.latitude); 
 	//const radius_in_miles = req.query.radius; 
-	const time = req.query.time * 3600; // hours to seconds
+	const time = parseFloat(req.query.time) * 3600; // hours to seconds
 	const walk_speed = 1.5; // average walking speed of 1.5 meters per second
 	const radius = time * walk_speed;
 	
@@ -185,7 +185,7 @@ locationRoutes.route("/walking").get(function(req, res){
 				$near:
 				{
 					$geometry: {type: "Point", coordinates: [long, lat]},
-					$maxDistance: parseFloat(radius)
+					$maxDistance: radius
 				}
 			}}
 		).toArray(function (err, result) {
@@ -201,7 +201,7 @@ locationRoutes.route("/walking").get(function(req, res){
 		});
 } );
 
-
+//compare locations by thier names
 function logical_and(list1, list2){
 	var combined_list = [];
 	for(var i =0 ; i<list1.length; ++i){
@@ -225,7 +225,7 @@ function combine_unique_masterList_and_newList(masterList, newList) {
 //may be void
 function clean_list(list){
 
-	let uniqueNames = []
+	let uniqueNames = [];
 	let uniqueList = [];
 	list.forEach((c) => {
 		if (!uniqueNames.includes(c.name)) {
@@ -302,7 +302,7 @@ locationRoutes.route("/checkforbus").get(function (req, res) {
                  $near:
                  {
                      $geometry: {type: "Point", coordinates: [long, lat]},
-                     $maxDistance: parseFloat(radius)
+                     $maxDistance: radius
                  }
              }}
             ).toArray(function (err, result) {
@@ -327,11 +327,10 @@ locationRoutes.route("/checkforbus").get(function (req, res) {
 
 
 //this is the function that will calculate a list of places based on a given transportation
- async function parseTransport(transportation, distance, time, coordinates){
+function parseTransport(transportation, distance, time, latitude, longitude){
 	 return new Promise((resolve, reject) =>{
 		const dbConnect = dbo.getDb();
 		const businesses = dbConnect.db('businesses').collection('locations');
-		businesses.createIndex( { coordinates : "2dsphere" } );
 		const long = parseFloat(longitude);
 		const lat = parseFloat(latitude); 
 	
@@ -351,7 +350,7 @@ locationRoutes.route("/checkforbus").get(function (req, res) {
 						$near:
 						{
 							$geometry: {type: "Point", coordinates: [long, lat]},
-							$maxDistance: parseFloat(radius)
+							$maxDistance: radius
 						}}
 					}
 				).toArray(function (err, result) {
@@ -399,7 +398,7 @@ locationRoutes.route("/checkforbus").get(function (req, res) {
 		})
 	 }
 	
-function parseBudget(budget, distance, coordinates){
+function parseBudget(budget, distance, latitude, longitude){
 	return new Promise((resolve, reject) =>{
 		const dbConnect = dbo.getDb();
 		const businesses = dbConnect.db('businesses').collection('locations');
@@ -435,7 +434,7 @@ function parseBudget(budget, distance, coordinates){
 	})
 }
 
-function parseTags(tags, distance, coordinates){
+function parseTags(tags, distance, latitude, longitude){
 	return new Promise((resolve, reject) =>{
 
 		const dbConnect = dbo.getDb();
@@ -473,17 +472,17 @@ function parseTags(tags, distance, coordinates){
 
 locationRoutes.route("/queryAll").get(async function (req, res) {
 
-	const transport_list = clean_list(await parseTransport(req.query.transportation, req.query.radius, req.query.time, req.query.coordinates)
+	const transport_list = clean_list(await parseTransport(req.query.transportation, req.query.radius, req.query.time, req.query.latitude, req.query.longitude)
 	.catch(err=>{
 		res.status(400).send("Error when querying transport");
 	})
 	)
-	const budget_list = clean_list(await parseBudget(req.query.budget, req.query.radius, req.query.coordinates)
+	const budget_list = clean_list(await parseBudget(req.query.budget, req.query.radius, req.query.latitude, req.query.longitude)
 	.catch(err=>{
 		res.status(400).send("Error when querying budget");
 	})
 	);
-	const tags_list = clean_list(await parseTags(req.query.tags, req.query.radius, req.query.coordinates)
+	const tags_list = clean_list(await parseTags(req.query.tags, req.query.radius, req.query.latitude, req.query.longitude)
 	.catch(err=>{
 		res.status(400).send("Error when querying tags");
 	})
